7983ab63de4331860f3681f472355e690413bcfb
Andy616 <53362130+Andy616@users.noreply.github.com>
2019-12-08 13:44:47 +0800

    version updated

--- spark_cluster/build-images.sh
+++ spark_cluster/build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -f dockerfile-spark-base -t spark-base:2.4.4 .
+docker build -f dockerfile -t spark-base:2.4.4 .
 docker build -f dockerfile-spark-master -t spark-master:2.4.4 .
 docker build -f dockerfile-spark-worker -t spark-worker:2.4.4 .
-docker build -f dockerfile-spark-submit -t spark-submit:2.4.4 .
+
